7410c476b4bb71edac8a4141f1c4cf1b8a4c9f0c
Ruth Winecoff <rlwinecoff@gmail.com>
2019-04-19 17:56:34 -0400

    Update packages_for_new_machine.R

--- packages_for_new_machine.R
+++ packages_for_new_machine.R
@@ -9,8 +9,28 @@ install.packages("lintr")
 # Apply suggested formatting to code
 install.packages("styler")
 
+# Reshape data
+install.packages("tidyr")
+
 # Extra string functions
 install.packages("stringr")
+
+# Pretty colors
+install.packages("RColorBrewer")
+
+# Make plots
+install.packages("ggplot2")
+
+# I don't know what this does yet
+install.packages("rmarkdown")
+
 library("lintr") # To use: lint("filename.R")
 library("styler") # To use: style_file("filename.R")
 library("stringr")
+library("tidyr")
+library("RColorBrewer")
+library("ggplot2")
+library("rmarkdown")
+
+
+
